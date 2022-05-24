4f0fd8cba471a58c517ec1f44ee0902ce60e6bf8
exuanbo <exuanbo@protonmail.com>
2022-05-24 20:09:24 +0100

    done with lab02

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -1,27 +1,33 @@
-#include <stdio.h>
 #include "bit_ops.h"
+
+#include <stdio.h>
 
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
-unsigned get_bit(unsigned x,
-                 unsigned n) {
-    // YOUR CODE HERE
-    // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns
-    // 0 or 1)
-    return -1;
+unsigned get_bit(unsigned x, unsigned n) {
+  // YOUR CODE HERE
+  // Returning -1 is a placeholder (it makes
+  // no sense, because get_bit only returns
+  // 0 or 1)
+  unsigned bitmask = 1 << n;
+  unsigned result = x & bitmask;
+  return result >> n;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
-void set_bit(unsigned * x,
-             unsigned n,
-             unsigned v) {
-    // YOUR CODE HERE
+void set_bit(unsigned* x, unsigned n, unsigned v) {
+  // YOUR CODE HERE
+  unsigned bitmask = 1 << n;
+  if (v == 0) {
+    *x = *x & ~bitmask;
+  } else {
+    *x = *x | bitmask;
+  }
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
-void flip_bit(unsigned * x,
-              unsigned n) {
-    // YOUR CODE HERE
+void flip_bit(unsigned* x, unsigned n) {
+  // YOUR CODE HERE
+  unsigned bitmask = 1 << n;
+  *x = *x ^ bitmask;
 }
-
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
-}
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void lfsr_calculate(uint16_t *reg) {
+  /* YOUR CODE HERE */
+  unsigned new_msb = (*reg & 1) ^ ((*reg >> 2) & 1) ^ ((*reg >> 3) & 1) ^ ((*reg >> 5) & 1);
+  *reg = (*reg >> 1) | (new_msb << 15);
+}
