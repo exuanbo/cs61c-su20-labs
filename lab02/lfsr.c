b3507ee3df40d274cf2a7c65f6a7e0f88972411e
exuanbo <exuanbo@protonmail.com>
2022-05-25 10:11:24 +0100

    simplify lab02 `lfsr`

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,6 +6,6 @@
 
 void lfsr_calculate(uint16_t *reg) {
   /* YOUR CODE HERE */
-  unsigned new_msb = (*reg & 1) ^ ((*reg >> 2) & 1) ^ ((*reg >> 3) & 1) ^ ((*reg >> 5) & 1);
+  unsigned new_msb = ((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1u;
   *reg = (*reg >> 1) | (new_msb << 15);
 }
