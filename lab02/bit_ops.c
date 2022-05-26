e1d3d1903c6a053d2a44b95bcf74ac6bb3321b15
exuanbo <exuanbo@protonmail.com>
2022-05-26 16:02:40 +0100

    insert newlines

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -12,6 +12,7 @@ unsigned get_bit(unsigned x, unsigned n) {
   unsigned result = x & bitmask;
   return result >> n;
 }
+
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned *x, unsigned n, unsigned v) {
@@ -23,6 +24,7 @@ void set_bit(unsigned *x, unsigned n, unsigned v) {
     *x = *x | bitmask;
   }
 }
+
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned *x, unsigned n) {
