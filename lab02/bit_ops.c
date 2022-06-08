b7b6b674539ad8d880ecbda437921e48f72ab3e2
exuanbo <exuanbo@protonmail.com>
2022-06-08 15:22:00 +0100

    minor refactor lab02

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -8,20 +8,18 @@ unsigned get_bit(unsigned x, unsigned n) {
   // Returning -1 is a placeholder (it makes
   // no sense, because get_bit only returns
   // 0 or 1)
-  unsigned bitmask = 1 << n;
-  unsigned result = x & bitmask;
-  return result >> n;
+  return (x >> n) & 1;
 }
 
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned *x, unsigned n, unsigned v) {
   // YOUR CODE HERE
-  unsigned bitmask = 1 << n;
+  int mask = 1 << n;
   if (v == 0) {
-    *x = *x & ~bitmask;
+    *x &= ~mask;
   } else {
-    *x = *x | bitmask;
+    *x |= mask;
   }
 }
 
@@ -29,6 +27,5 @@ void set_bit(unsigned *x, unsigned n, unsigned v) {
 // Assume 0 <= n <= 31
 void flip_bit(unsigned *x, unsigned n) {
   // YOUR CODE HERE
-  unsigned bitmask = 1 << n;
-  *x = *x ^ bitmask;
+  *x ^= (1 << n);
 }
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,6 +6,6 @@
 
 void lfsr_calculate(uint16_t *reg) {
   /* YOUR CODE HERE */
-  unsigned new_msb = ((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1u;
+  int new_msb = ((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1;
   *reg = (*reg >> 1) | (new_msb << 15);
 }
